714e93457cd831316e9ff8f7dabdd55d9a78cd6a
sumanworkspace-crypto <sumanworkspace39@gmail.com>
2026-09-14 11:54:52 +0530

    Add build.gradle.kts with Gradle plugin and Maven publishing

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-gradle-plugin`
+    `maven-publish`
+}
+
+group = "com.liquid.ide.tooling"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Gradle API is provided by the gradle runtime
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+gradlePlugin {
+    plugins {
+        create("liquidToolingPlugin") {
+            id = "com.liquid.ide.tooling"
+            implementationClass = "com.liquid.ide.tooling.LiquidToolingPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPages"
+            url = uri("${rootDir}/maven")
+        }
+    }
+}
